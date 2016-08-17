36a082879c5dc3ae10bed73185d73dd6c97cec20
arunroy <arun.iamwatiam@gmail.com>
2016-08-17 22:38:23 +0100

    file rename

---
+++ public/javascripts/userLookup.js
@@ -0,0 +1,69 @@
+/*Interface Practical Test 1*/
+
+var employee;
+
+document.getElementById("searchTxt").onkeyup=getData;
+fetchData();
+
+document.getElementById("nextBtn").addEventListener("click", function(event){
+    event.preventDefault();
+});
+
+document.getElementById("prevBtn").addEventListener("click", function(event){
+    event.preventDefault();
+});
+
+function getData(e){
+	
+	var queryString= document.getElementById('searchTxt').value;
+	var positions=[];
+	var filteredArray=[];
+	var flag=false;
+
+	//iterate the entire data set and see if the particular dataset is present
+	for(var i=0;i<employee.length;i++){
+		if(employee[i]['name'].toLowerCase().indexOf(queryString)>=0){
+			flag=true;
+			positions.push(i);
+
+		}
+	}
+
+
+	for(var i=0;i<positions.length;i++){
+		filteredArray.push(employee[positions[i]]);
+	}
+	
+	var headers=['Name','Age','Shoe size','Title'];
+	var fields=['name','age','shoe_size','title'];
+	var table=generateTable(filteredArray, headers, fields);
+	//alert(table);
+	
+	var dvTable = document.getElementById("userTable");
+	dvTable.innerHTML = "";
+	dvTable.appendChild(table);
+}
+
+
+function fetchData(){
+	
+	getAjaxData('master_data.json', 'get', function(obj){
+		
+		emp=obj.responseText;
+		//console.log(emp);
+		
+		employee=JSON.parse(emp);
+		
+		var headers=['Name','Age','Shoe size','Title'];
+		var fields=['name','age','shoe_size','title'];
+		var table=generateTable(employee, headers, fields);
+		//alert(table);
+
+		var dvTable = document.getElementById("userTable");
+		dvTable.innerHTML = "";
+		dvTable.appendChild(table);
+		});	
+}
+
+
+
