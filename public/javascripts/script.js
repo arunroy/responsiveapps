7880c57ad27f08abcb15ab7da11ebfc5fdc4d797
arunroy <arun.iamwatiam@gmail.com>
2016-08-25 12:40:27 +0100

    has moved the basic scripts to separate file

---
+++ public/javascripts/script.js
@@ -0,0 +1,19 @@
+        function displayNextItem(){
+			document.getElementById('lookupDiv').style.display = 'block';
+			document.getElementById('pivotTableDiv').style.display = 'none';
+		}
+				
+	    function displayNextItem(){			
+				document.getElementById('lookupDiv').style.display = 'none';
+				document.getElementById('pivotTableDiv').style.display = 'block';
+        }
+		
+		function focusSearchBox(){
+				document.getElementById('searchTxt').value='';
+			    document.getElementById('searchTxt').className="searchTxtActive";
+		}
+		
+		function blurSearchBox(){
+				document.getElementById('searchTxt').value='Search';
+			    document.getElementById('searchTxt').className="searchTxt";
+		}
